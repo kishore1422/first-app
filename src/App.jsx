345a0afe8a652b6d932b15536585a1392e0b432d
Nanda kishore <nandakishore14n@gmail.comm>
2026-04-25 06:54:32 +0530

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1> Hello I am  Nanda kishore  </h1>
+     <p> This is my first react app </p>
+    </>
+  )
+}
+
+export default App
