367e16b1770e359de81d2d1ccd4d5679e40be291
Nanda kishore <nandakishore14n@gmail.comm>
2026-05-03 11:18:08 +0530

    adding balll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 
 import './App.css'
+import { motion } from 'motion/react'
 
 function App() {
 
   return (
     <>
+      <motion.div
+     
+      animate = {{
+        x : [0,1200,1200,0,0],
+        y : [0,0,700,700,0],
+        rotate : 360
+      }}
+      transition={{
+        delay: 1 ,
+        duration : 5,
+        repeat: Infinity,
+        ease: "anticipate"
+      }}
+       id='box'
+       ></motion.div>
      <h1 className='text-3xl m-1 font-bold p-4 border-2 text-center text-cyan-300 backdrop-blur-2xl'> Hello I am  Nanda kishore  </h1>
      <p className='text-center'> I am learning React </p>
      <br></br>
@@ -15,8 +31,12 @@ function App() {
         <h1 className=' text-cyan-400 bg-blend-color text-xl rounded-3xl m-1 p-2'> This is Mother and Daughter</h1>
         <p className=' text-black text-sm text-center'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat et perferendis sapiente maxime repudiandae inventore alias, dolores, consequatur vero laboriosam consectetur hic recusandae provident ad dolore voluptate aperiam ullam?
         </p>
+
       </div>
      </div>
+     <hr></hr>
+     <br></br>
+   
     </>
   )
 }
